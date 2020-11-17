321ba667ee7fd22ffdb5368a8773d0771d03564f
NDK1410 <khanh.nd1410@gmail.com>
2020-11-16 20:56:22 -0800

    runApp

---
+++ lib/src/MyApp.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+import 'package:f10GridViewExample/src/pages/HomePage.dart';
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: HomePage(),
+    );
+  }
+}
